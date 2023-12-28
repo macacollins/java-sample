d8864014d676d5fcf349b467c9e3742c13a6a113
Mac Collins <macacollins@gmail.com>
2023-12-28 14:52:44 -0600

    Add prometheus metrics and timing to JRE and the fibonacci endpoint.

--- java/src/main/java/com/macacollins/spring/sample/FibonacciController.java
+++ java/src/main/java/com/macacollins/spring/sample/FibonacciController.java
@@ -3,6 +3,13 @@ package com.macacollins.spring.sample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.annotation.Timed;
 
 import com.macacollins.spring.sample.fibonacci.FibonacciFactory;
 import com.macacollins.spring.sample.fibonacci.FibonacciGenerator;
@@ -10,21 +17,47 @@ import com.macacollins.spring.sample.fibonacci.FibonacciGenerator;
 @RestController
 public class FibonacciController {
 
+    // This should maybe be injected in a ServletFilter somewhere instead
+    private MeterRegistry registry;
+
+    public FibonacciController(MeterRegistry registry) {
+        this.registry = registry;
+    }
+
+    private static final String METRIC_NAME = "fibonacci.status";
+
     // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ....
     // GET is appropriate because this method can be cached
     @GetMapping("/fibonacci")
+    @Timed(
+            value = "fibonacci.get",
+            description = "Time to calculate fibonacci values",
+            percentiles = { 0.05, 0.1, 0.2, 0.5, 0.75, 0.9, 0.99, 0.999 }
+    )
     public String getFibonacciNumber(
-            @RequestParam(name = "sequenceNumber", defaultValue = "0") String sequenceNumber) {
-        String result = "-1";
+            @RequestParam(name = "sequenceNumber") String sequenceNumber) {
+
+        String result;
 
         try {
             FibonacciGenerator generator = FibonacciFactory.getGenerator();
 
             result = generator.calculateFibonacci(Integer.valueOf(sequenceNumber));
         } catch (NumberFormatException e) {
-            // TODO return 500 result here
+            registry.counter(METRIC_NAME, "status", "400").increment();
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Invalid sequence number supplied: " + sequenceNumber);
         }
 
+        if (result.startsWith("-")) {
+            // This is a rough way to check for negativity, but it needs to work with any of the FibonacciGenerators
+            // This won't catch results that overflow back to positive. Limited precision numbers are bad for this
+            registry.counter(METRIC_NAME, "status", "500").increment();
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Integer overflow detected--returning 500");
+        }
+
+        registry.counter(METRIC_NAME, "status", "200").increment();
         return result;
     }
 }
--- java/src/main/java/com/macacollins/spring/sample/SampleApplication.java
+++ java/src/main/java/com/macacollins/spring/sample/SampleApplication.java
@@ -1,13 +1,27 @@
 package com.macacollins.spring.sample;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	/**
+	 * This is required so that we can use the @Timed annotation
+	 * on methods that we want to time.
+	 * See: https://micrometer.io/docs/concepts#_the_timed_annotation
+	 */
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 
 }
